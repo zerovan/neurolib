ec5f35c50c6846efcd769a360f62b5e88283fb27
zerovan <mohammad.sh.1370@gmail.com>
2025-11-20 01:50:06 +0100

    wc with delay

--- neurolib2/models/wilsonCowanModel/wilsonCowanModel.py
+++ neurolib2/models/wilsonCowanModel/wilsonCowanModel.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 
 class WilsonCowan(BaseModel):
 
+    tau_EI: jax.Array
+    tau_IE: jax.Array
     tau_e: jax.Array
     tau_i: jax.Array
     w_ee: jax.Array
@@ -28,6 +30,8 @@ class WilsonCowan(BaseModel):
         dt: float = 0.1,
     ):
         super().__init__(state=state, dt=dt)
+        self.tau_EI = jnp.array(1.0)
+        self.tau_IE = jnp.array(2.0)
         self.tau_e = jnp.array(0.1)
         self.tau_i = jnp.array(0.5)
         self.w_ee = jnp.array(16.0)
@@ -50,28 +54,49 @@ class WilsonCowan(BaseModel):
     def _S_i(self, x: jnp.ndarray) -> jnp.ndarray:
         return self._logistic(x, self.a_i, self.theta_i)
 
-    def wc_vector_field(self, t, y, args):
+
+
+    def wc_vector_field_with_delay(self, t, y, args, *, history):
         E, I = y
+        # E_delayed, I_delayed = history[0]
+        # input_e = self.w_ee * E_delayed - self.w_ei * I + self.P_e
+        # input_i = self.w_ie * E - self.w_ii * I_delayed + self.P_i
 
-        input_e = self.w_ee * E - self.w_ei * I + self.P_e
-        input_i = self.w_ie * E - self.w_ii * I + self.P_i
 
+        delayed_EI, delayed_IE = history
+
+        E_tau_EI = delayed_EI[0]   # E(t - tau_EI)
+        I_tau_IE = delayed_IE[1]   # I(t - tau_IE)
+
+        input_e = self.w_ee * E - self.w_ei * I_tau_IE + self.P_e
+        input_i = self.w_ie * E_tau_EI - self.w_ii * I + self.P_i
+        
         S_e = self._S_e(input_e)
         S_i = self._S_i(input_i)
 
         dE = (-E + S_e) / self.tau_e
         dI = (-I + S_i) / self.tau_i
-
         return jnp.array([dE, dI])
 
+    def history_fn(self, t):
+        return jnp.array([0.1, 0.1])
+
     
-    def simulate(self, duration=50.0, steps=2000):
+    def simulate(self, duration=50.0, steps=1000):
         t0, t1 = 0.0, duration
         ts = jnp.linspace(t0, t1, steps)
 
-        term = diffrax.ODETerm(self.wc_vector_field)
-        solver = diffrax.Kvaerno5()
+        term = diffrax.ODETerm(self.wc_vector_field_with_delay)
+        solver = diffrax.Bosh3()
 
+
+        delays = diffrax.Delays(
+            delays=[
+                lambda t, y, args: self.tau_EI,   # first delay for E→I
+                lambda t, y, args: self.tau_IE,   # second delay for I→E
+            ],
+            initial_discontinuities=jnp.array([0.0])
+        )
 
         sol = diffrax.diffeqsolve(
             term,
@@ -79,13 +104,14 @@ class WilsonCowan(BaseModel):
             t0=t0,
             t1=t1,
             dt0=0.01,
-            y0=self.state,
-            args=None,                     # we don't need extra args
-            saveat=diffrax.SaveAt(ts=ts),
+            y0=self.history_fn,
+            args=None,
+            saveat=diffrax.SaveAt(ts=ts, dense=True),
             stepsize_controller=diffrax.PIDController(
-                rtol=1e-3,  # good default
+                rtol=1e-3,  
                 atol=1e-6,
             ),
+            delays=delays,
             max_steps=5_000_000
         )
         return sol.ts, sol.ys
