fd98a7e07cbd23e0c0b33c6ea041b6f7a5ff94f8
zerovan <mohammad.sh.1370@gmail.com>
2026-01-08 10:36:17 +0100

    sample OU noise inside dynamics()

--- neurolib2/examples/wilsonCowanExample.py
+++ neurolib2/examples/wilsonCowanExample.py
@@ -15,8 +15,8 @@ from neurolib2.models.wilsonCowanModel.wilsonCowanModel import WilsonCowan
 
 # Example: run simulation when module executed as script
 if __name__ == "__main__":
-    model = WilsonCowan.create_default(dt=0.01, fiber_length_matrix=ds.Dmat, fiber_count_matrix=ds.Cmat)
-    # model = WilsonCowan.create_default(dt=0.01)
+    # model = WilsonCowan.create_default(dt=0.01, fiber_length_matrix=ds.Dmat, fiber_count_matrix=ds.Cmat)
+    model = WilsonCowan.create_default(dt=0.01)
     times, states = model.simulate(2)
     model.plot(times, states)
     print(states)
